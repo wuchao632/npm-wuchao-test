9f6b877f4e7f139cf75ebd9227e08f8cafa82311
wuchao <86945403@qq.com>
2020-07-09 14:04:02 +0800

    2020-7-9 2:04:02 ├F10: PM┤

--- push.js
+++ push.js
@@ -1,4 +1,4 @@
 const shell = require('shelljs');
 shell.exec('git add .');
-shell.exec('git commit -m');
-shell.exec('git push -u origin master');
+shell.exec(`git commit -m ${JSON.stringify(new Date().toLocaleString())}`);
+shell.exec(`git push -u origin master`);
